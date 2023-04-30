c5d26bf990f6217b5222b378074780a44fb1aced
Raju Islam <bdrajuislam246@gmail.com>
2023-04-30 12:21:04 +0600

    retrive product from stored cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -16,7 +16,27 @@ const Shop = () => {
 
     useEffect(()=>{
         const storedCart = getShoppingCart();
-    },[])
+        const saveCart =[];
+        // s1: get id
+        for (const id in storedCart){
+            // s2: get the product using id
+            const saveProduct = products.find(product =>product.id == id)
+           
+            console.log(saveProduct);
+            // s3: get quantity of te product
+            if(saveProduct){
+                const quantity = storedCart[id];
+                saveProduct.quantity =quantity;
+                s4 : add the added product to the saved cart
+                saveCart.push(saveProduct)
+            }
+            
+            
+
+
+        }
+        setCart(saveCart)
+    },[products])
     
     const hendleAddToCart = (product) =>{
         // cart.push(product);
